b9de1876f3b15c5de9111dff1353bfef57da670a
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-04-16 20:11:06 +0530

    table calculator

--- tables/main.rs
+++ tables/main.rs
@@ -3,10 +3,23 @@ use std::io;
 
 fn main(){
 
-    let mut n= 4;
+    let mut n= String::new();
     println!("Enter the number");
     io::stdin().read_line(&mut n)
         .expect("Failed to read line");
 
-    println!("{}", n);
+    let n:usize = n.trim().parse()
+        .expect("Please type a number!");
+
+    let mut table :[[usize;2];10] = [[0;2];10];
+
+    for i in 0..10{
+        table[i][0] = i+1;
+        table[i][1] = n * table[i][0];
+    }
+
+    for i in 0..10{
+        println!("{} x {} = {}", n, table[i][0], table[i][1]);
+    }
+
 }
