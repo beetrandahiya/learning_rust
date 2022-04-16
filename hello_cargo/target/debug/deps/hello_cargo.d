ce0d1a8a4a28bcca748774cfbf8f156e317ef73f
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-04-16 20:14:51 +0530

    hello cargo shit

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+c:\Work\RUST\learning_rust\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+c:\Work\RUST\learning_rust\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+c:\Work\RUST\learning_rust\hello_cargo\target\debug\hello_cargo.exe: c:\Work\RUST\learning_rust\hello_cargo\src\main.rs
